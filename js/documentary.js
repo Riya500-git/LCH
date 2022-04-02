6982ef2138d4cb1ad18c7a1347c374bcaa6f4a1c
Muhammad Talha <sulemanshah432@gmail.com>
2022-04-02 16:29:03 +0530

    documentary page

---
+++ js/documentary.js
@@ -0,0 +1,41 @@
+var swiper = new Swiper(".swiper", {
+  spaceBetween: 10,
+  loop:true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 9500,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+/*   breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    450: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    1024: {
+      slidesPerView: 4,
+    },
+  }, */
+});
+
+var swiper = new Swiper(".swiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  loop:true,
+});
